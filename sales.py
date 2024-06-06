d727fcca3f1b31b4fecd6dab42e4386b8f6805d2
Abena Adu <abeanadu@gmail.com>
2024-06-06 16:46:48 -0400

    Cleaning data

--- sales.py
+++ sales.py
@@ -105,3 +105,16 @@ Eric Mcdaniel ;,;$29.70;,; green ;,; 09/15/17
 green&white;,;09/15/17,   Gail Phelps   ;,;$30.52   
 ;,; green&white&blue   ;,; 09/15/17 , Myrtle Morris 
 ;,;   $22.66   ;,; green&white&blue;,;09/15/17"""
+
+# Replaced artifacts with periods to easily separate data details
+daily_sales_replaced = daily_sales.replace(";,;", "&")
+print(daily_sales_replaced)
+
+#Splitting data via commas
+daily_transactions = daily_sales_replaced.split(",")
+
+#Splitting data via "&" character and creating a new list 
+daily_transactions_split = []
+
+for transaction in daily_transactions:
+  daily_transactions_split.append(transaction.split("&"))
