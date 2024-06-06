eece16c29026f577a0b18ec7b76e3253438aa838
Abena Adu <abeanadu@gmail.com>
2024-06-06 18:24:10 -0400

    Determined total sales

--- sales.py
+++ sales.py
@@ -136,3 +136,10 @@ for transaction in transactions_clean:
   customers.append(transaction[0])
   sales.append(transaction[1])
   thread_sold.append(transaction[2])
+
+#Determining Total Sales 
+total_sales = 0
+
+for sale in sales:
+  total_sales += float(sale.strip("$"))
+  
